55207236cf9b47a6d473ebc8b6334fd7b071889b
Ralf Hallmen <ralf.hallmen@gmail.com>
2017-06-27 23:46:39 +0200

    added class log
    - approx_log moved to class log
    - scaling works now

--- log.py
+++ log.py
@@ -23,22 +23,40 @@ def find_msb(number):
     x = math.floor(math.log2(number))
     return x
 
-def approx_log2(number):
-    msb = find_msb(number)
-    rest = number ^ (1<<msb)
-    shift_rest = rest / 2**msb
-    # use 8 bit lut
-    bits = 5
-    lut = get_lut(5, bits)
-    # lookup value must be shifted by number of lut bits
-    lookup = int(2**5 * shift_rest)
-    dec = lut[lookup]
-    print(msb, rest, lookup, dec)
-    return 2**5*(msb)+dec
+class log(object):
+    def __init__(self, input_bits, output_bits):
+        # compile time constants
+        self.lut_bits = 5
+        assert input_bits > 1
+        assert output_bits > self.lut_bits
+        self.in_bits = input_bits
+        self.out_bits = output_bits
+        max_val = ((self.in_bits-1) << self.lut_bits) + 2**self.lut_bits-1
+        self.scale = (2**self.out_bits-1) / max_val
+
+    def approx_log2(self, number):
+        msb = find_msb(number)
+        rest = number ^ (1<<msb)
+        shift_rest = rest / 2**msb
+        # use 8 bit lut
+        bits = 5
+        lut = get_lut(bits, self.lut_bits)
+        # lookup value must be shifted by number of lut bits
+        lookup = int(2**bits * shift_rest)
+        dec = lut[lookup]
+        print(msb, rest, lookup, dec)
+        result = 2**self.lut_bits*msb + dec
+        return result * self.scale
 
 
 if __name__ == "__main__":
+    approx_log2 = log(8,8).approx_log2
     for num in range(1,33):
         expected = normal_log2(num) * 256 / normal_log2(256)
         actual = approx_log2(num)
-        print("{} {}".format(expected, actual))
+        diff = math.fabs(expected-actual)
+        print("{} {} {}".format(expected, actual, diff))
+    approx_log2 = log(18,18).approx_log2
+    expected = normal_log2(2**18-1) * (2**18-1) / normal_log2(2**18-1)
+    actual = approx_log2(2**18-1)
+    print("{} {}".format(expected, actual))
