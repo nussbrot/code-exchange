4726181ba84f123e6cb729f2cb277301a4110f76
Ralf Hallmen <ralf.hallmen@gmail.com>
2017-11-15 22:36:47 +0100

    added tree example

--- python_schools/iteration.py
+++ python_schools/iteration.py
@@ -21,6 +21,7 @@ iterator = iterator_protocol(1, "Suppe", None, 3.12)
 for item in iterator:
     print(item)
 
+
 # the same iterator implemented as generator function
 def generator(*args):
     for item in args:
@@ -29,9 +30,16 @@ def generator(*args):
 for item in generator(1, "Suppe", None, 3.12):
     print(item)
 
+
+# Generators can also be created by putting list comprehensions in round brackets
+print("\nSquares")
+squares = (x*x for x in range(10))
+for item in squares:
+    print(item)
+
+
 # iterators also work recursively.
 # let's take a look at Guido's binary tree inorder traversal:
-
 def inorder(t):
     if t:
         for x in inorder(t.left):
@@ -39,3 +47,21 @@ def inorder(t):
         yield t.dat
         for x in inorder(t.right):
             yield x
+
+class tree(object):
+    def __init__(self, dat, left=None, right=None):
+        self.dat = dat
+        self.left = left
+        self.right = right
+
+# A small test tree:
+#    10
+#    /\
+#   7  13
+#  /\
+# 5 9
+my_tree = tree(10, tree(7, tree(5), tree(9)), tree(13))
+
+print("\nBinary tree traversal")
+for node in inorder(my_tree):
+    print(node)
